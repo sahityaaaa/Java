bf8d2aa32f78d36087d2fe41c42669b2fb387eff
sahityaaaa <100367450+sahityaaaa@users.noreply.github.com>
2022-03-28 22:10:23 +0530

    Add files via upload

---
+++ Phase1/Phase-1 Lesson-3/KeyWordCustomException/src/com/keerthi/CustomExceptionDemo.java
@@ -0,0 +1,28 @@
+package com.keerthi;
+
+class MyException extends Exception 
+{ 
+ public MyException(String s) 
+ { 
+ super(s); 
+ } 
+} 
+
+
+public class CustomExceptionDemo {
+
+	public static void main(String[] args) {
+		try
+		 { 
+		 throw new MyException("temp"); 
+		 } 
+		 catch (MyException ex) 
+		 { 
+		 System.out.println("Caught"); 
+		 System.out.println(ex.getMessage()); 
+		 } 
+
+
+	}
+
+}
